00206d60765edc9160917b4f71641ed941e3c8b6
kohina.pandey <kohina.pandey@accolitedigital.com>
2024-01-08 15:02:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConverterComponent } from './converter/converter.component';
+import { FormsModule } from '@angular/forms';
+import { NumberToWordsPipe } from './number-to-words.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConverterComponent,
+    NumberToWordsPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [NumberToWordsPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NumberToWordsPipe } from '../number-to-words.pipe'; 
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent {
+  inputNumber!: number;
+  outputText!: string;
+
+  constructor(private numberToWords: NumberToWordsPipe) {}
+
+  convert() {
+    console.log("convert");
+    if (!this.isRange() || !this.isValidNumber()) {
+      return;
+    }
+
+    
+    this.outputText = this.numberToWords.transform(this.inputNumber);
+  }
+
+  isRange(): boolean {
+    if (this.inputNumber > 1000000) {
+      console.log("Invalid range detected");
+      this.outputText = 'Please enter a value less than or equal to 10 lakhs.';
+      return false;
+    }
+
+    return true;
+  }
+
+  isValidNumber(): boolean {
+    if (isNaN(this.inputNumber) || this.inputNumber === null) {
+      console.log("Invalid number detected");
+      this.outputText = 'Please enter a valid number';
+      return false;
+    }
+
+    return true;
+  }
+}
